bb7b158ca2c3fad7e491c677c87c07a1272431db
Jeff <jeff999955@gmail.com>
2024-01-24 00:31:22 -0800

    Also set seed in render

--- render.py
+++ render.py
@@ -57,10 +57,11 @@ if __name__ == "__main__":
     parser.add_argument("--skip_train", action="store_true")
     parser.add_argument("--skip_test", action="store_true")
     parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--seed", type=int, default = 0)
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
 
     # Initialize system state (RNG)
-    safe_state(args.quiet)
+    safe_state(args.quiet, args.seed)
 
     render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test)
