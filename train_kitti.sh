0f6e356132189fd34783ffe3ee78a28e54141cab
Jeff <jeff999955@gmail.com>
2024-03-04 16:36:40 -0800

    Change saved exp name to a variable

--- train_kitti.sh
+++ train_kitti.sh
@@ -5,9 +5,11 @@ DIR="/ubc/cs/research/kmyi/wsun/scratch/dataset/kitti360"
 SCANS=("00" "01" "02" "03" "04")
 ITERS=("5000" "10000" "15000" "20000" "25000" "30000")
 
+EXP_NAME="kitti_random"
+
 for scan in ${SCANS[@]}; do
-    echo python train.py -s $DIR \
-        -m ./exp/kitti/train_$scan \
+    python train.py -s $DIR \
+        -m ./exp/$EXP_NAME/train_$scan \
         -i $DIR/data_2d_nvs_drop50/train_$scan.txt \
         --data-device cpu \
         --iterations 30000 \
@@ -15,21 +17,21 @@ for scan in ${SCANS[@]}; do
 
     for iter in ${ITERS[@]}; do
         python render.py -s $DIR \
-            -m ./exp/kitti/train_$scan \
+            -m ./exp/$EXP_NAME/train_$scan \
             -i $DIR/data_2d_nvs_drop50/train_$scan.txt \
             --data-device cpu \
             --iteration $iter 
 
         python /jeffyct-ssd/projects/GS-Collection/evaluate.py \
-            -i ./exp/kitti/train_$scan/train/ours_$iter/renders \
-            -g ./exp/kitti/train_$scan/train/ours_$iter/gt \
+            -i ./exp/$EXP_NAME/train_$scan/train/ours_$iter/renders \
+            -g ./exp/$EXP_NAME/train_$scan/train/ours_$iter/gt \
             -m psnr ssim lpips vgglpips \
-            --prefix "train_${scan}_${iter}_train" | tee -a kitti.csv
+            --prefix "train_${scan}_${iter}_train" | tee -a $EXP_NAME.csv
 
         python /jeffyct-ssd/projects/GS-Collection/evaluate.py \
-            -i ./exp/kitti/train_$scan/test/ours_$iter/renders \
-            -g ./exp/kitti/train_$scan/test/ours_$iter/gt \
+            -i ./exp/$EXP_NAME/train_$scan/test/ours_$iter/renders \
+            -g ./exp/$EXP_NAME/train_$scan/test/ours_$iter/gt \
             -m psnr ssim lpips vgglpips \
-            --prefix "train_${scan}_${iter}_test" | tee -a kitti.csv
+            --prefix "train_${scan}_${iter}_test" | tee -a $EXP_NAME.csv
     done
 done
